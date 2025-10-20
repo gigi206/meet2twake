8d736862e8eecf6ce01a51d7e6eb5dcac351ed07
Bruno Michel <bruno@cozycloud.cc>
2025-10-20 10:57:17 +0200

    feat: Start http server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func minio(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	port := 8090
+	if env := os.Getenv("PORT"); env != "" {
+		p, err := strconv.Atoi(env)
+		if err != nil {
+			panic(err)
+		}
+		port = p
+	}
+	http.HandleFunc("/minio", minio)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
